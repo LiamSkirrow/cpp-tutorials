04d6466a8f8fa1458659059a6a4be98a90e940d0
Liam Skirrow <skirrowliam@gmail.com>
2024-09-01 12:45:31 +0100

    Added another example to pointers

--- C/pointers/main.c
+++ C/pointers/main.c
@@ -2,24 +2,22 @@
 
 #include <stdio.h>
 
-typedef struct dataStruct{
-    int var;
-} ds;
-
 int main(int argc, char **argv){
     
     int var = 6;
-    int *ptr;
+    int *ptr1, *ptr2;
 
-    ptr = &var;
+    ptr1 = &var;
+    ptr2 = ptr1;
 
-    ds blah;
-    blah.var = 5;
-
-    printf("&ptr = %x\n", &ptr);
-    printf("ptr = %x\n", ptr);
+    printf("&ptr1 = %x\n", &ptr1);
+    printf("ptr1 = %x\n", ptr1);
     printf("&var = %x\n", &var);
-    printf("var = %d\n", *ptr);
+    printf("var = %d\n\n", *ptr1);
+
+    printf("&ptr2 = %x\n", &ptr2);
+    printf("ptr2 = %x\n", ptr2);
+    printf("var = %d\n\n", *ptr2);
     
     return 0;
 }
